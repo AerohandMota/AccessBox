facf4781ab17c7f5daf2df91de2e994cac7a4f3b
MOTOYAMA Takefumi <bamboooo0130@gmail.com>
2020-09-27 22:26:09 +0900

    Initial Commit

---
+++ AccessBox_final/AccessBox_finalApp.swift
@@ -0,0 +1,20 @@
+//
+//  AccessBox_finalApp.swift
+//  AccessBox_final
+//
+//  Created by 本山武文 on 2020/09/27.
+//
+
+import SwiftUI
+
+@main
+struct AccessBox_finalApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
