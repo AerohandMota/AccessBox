1f2116a760b81c630579d7265a4e2e185413c865
MOTOYAMA Takefumi <bamboooo0130@gmail.com>
2020-09-28 02:08:55 +0900

    ObservableObject設定完了LoginView実装Start

---
+++ AccessBox_final/Datas.swift
@@ -0,0 +1,43 @@
+//
+//  Datas.swift
+//  AccessBox_final
+//
+//  Created by 本山武文 on 2020/09/28.
+//
+
+import Foundation
+import Combine
+
+class UserData: ObservableObject {
+    @Published var password: String? {
+        didSet {
+            UserDefaults.standard.set(password, forKey: "password")
+        }
+    }
+    @Published var passIsActive: Bool {
+        didSet {
+            UserDefaults.standard.set(passIsActive, forKey: "passIsActive")
+        }
+    }
+    @Published var faceIDIsActive: Bool {
+        didSet {
+            UserDefaults.standard.set(faceIDIsActive, forKey: "faceIDIsActive")
+        }
+    }
+    @Published var isFirstLaunch: Bool {
+        didSet {
+            UserDefaults.standard.set(isFirstLaunch, forKey: "isFirstLaunch")
+        }
+    }
+    init() {
+        self.password = UserDefaults.standard.string(forKey: "password")
+        self.passIsActive = UserDefaults.standard.bool(forKey: "passIsActive")
+        self.faceIDIsActive = UserDefaults.standard.bool(forKey: "faceIDIsActive")
+        self.isFirstLaunch = UserDefaults.standard.bool(forKey: "isFirstLaunch")
+    }
+}
+
+class SystemState: ObservableObject {
+    @Published var isUnlocked: Bool = false
+    @Published var isSystem: Bool = false
+}
---
+++ AccessBox_final/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  AccessBox_final
+//
+//  Created by 本山武文 on 2020/09/28.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
